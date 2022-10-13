614d3b926d3998ec0c93d38cc074bb8d95e8cfe6
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-10-13 12:53:12 -0300

    learning split shimming

---
+++ 9.2 ProductionBuild_Split_Shimming/src/libs/showDate.js
@@ -0,0 +1,8 @@
+
+export function showDate() {
+  const date = mnt(new Date()).format("DD-MM-YYYY");
+  console.log(
+    "turboCL -> file: showDate.js -> line 4 -> showDate -> date",
+    date
+  );
+}
